7361e7bb39a4c1f4cd990a19d0af6cf950345034
jstyle5 <54833010+jstyle5@users.noreply.github.com>
2020-08-16 16:20:51 +0900

    Update ShowMoreText.js

--- src/ShowMoreText.js
+++ src/ShowMoreText.js
@@ -3,144 +3,137 @@ import { PropTypes } from 'prop-types';
 import Truncate from './Truncate';
 
 class ShowMoreText extends Component {
+  _isMounted = false;
 
-    _isMounted = false;
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      expanded: false,
+      truncated: false,
+    };
+  }
 
-        this.state = {
-            expanded: false,
-            truncated: false
-        };
+  static defaultProps = {
+    lines: 3,
+    more: 'Show more',
+    less: 'Show less',
+    anchorClass: '',
+    onClick: undefined,
+    initialExpanded: false,
+    expanded: false,
+    width: 0,
+    keepNewLines: false,
+  };
+
+  static propTypes = {
+    children: PropTypes.node,
+    lines: PropTypes.number,
+    more: PropTypes.node,
+    less: PropTypes.node,
+    anchorClass: PropTypes.string,
+    onClick: PropTypes.func,
+    initialExpanded: PropTypes.bool,
+    expanded: PropTypes.bool,
+    width: PropTypes.number,
+    keepNewLines: PropTypes.bool,
+  };
+
+  componentDidMount() {
+    this._isMounted = true;
+
+    var _self = this;
+    if (this._isMounted) {
+      this.setState({
+        expanded: _self.props.initialExpanded,
+      });
     }
+  }
 
-    static defaultProps = {
-        lines: 3,
-        more: 'Show more',
-        less: 'Show less',
-        anchorClass: '',
-        onClick: undefined,
-        expanded: false,
-        width: 0,
-        keepNewLines: false
-    };
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-    static propTypes = {
-        children: PropTypes.node,
-        lines: PropTypes.number,
-        more: PropTypes.node,
-        less: PropTypes.node,
-        anchorClass: PropTypes.string,
-        onClick: PropTypes.func,
-        expanded: PropTypes.bool,
-        width: PropTypes.number,
-        keepNewLines: PropTypes.bool
-    };
+  handleTruncate = (truncated) => {
+    if (this._isMounted && truncated !== this.state.truncated) {
+      this.setState({
+        truncated,
+      });
+      if (truncated) {
+        this.truncateRef.onResize();
+      }
+    }
+  };
 
-    componentDidMount() {
-        this._isMounted = true;
-
-        var _self = this;
-        if (this._isMounted) {
-            this.setState({
-                expanded: _self.props.expanded
-            });
+  toggleLines = (event) => {
+    event.preventDefault();
+    var _self = this;
+    if (this._isMounted) {
+      this.setState(
+        {
+          expanded: !this.props.expanded,
+        },
+        () => {
+          if (_self.props.onClick) {
+            _self.props.onClick(_self.props.expanded);
+          }
         }
+      );
     }
+  };
 
-    componentWillUnmount() {
-        this._isMounted = false;
-    }
+  render() {
+    const {
+      children,
+      more,
+      less,
+      lines,
+      anchorClass,
+      width,
+      keepNewLines,
+    } = this.props;
 
-    handleTruncate = truncated => {
-        if (this._isMounted && truncated !== this.state.truncated) {
-            this.setState({
-                truncated
-            });
-            if (truncated) { this.truncateRef.onResize(); }
-        }
-    };
+    const { expanded, truncated } = this.state;
 
-    toggleLines = event => {
-        event.preventDefault();
-        var _self = this;
-        if (this._isMounted) {
-            this.setState(
-                {
-                    expanded: !this.state.expanded
-                },
-                () => {
-                    if (_self.props.onClick) {
-                        _self.props.onClick(_self.state.expanded);
-                    }
+    return (
+      <div>
+        <Truncate
+          width={width}
+          lines={!expanded && lines}
+          ellipsis={
+            <span>
+              ...{' '}
+              <a href='#' className={anchorClass} onClick={this.toggleLines}>
+                {more}
+              </a>
+            </span>
+          }
+          onTruncate={this.handleTruncate}
+          ref={(ref) => (this.truncateRef = ref)}
+        >
+          {keepNewLines
+            ? children.split('\n').map((line, i, arr) => {
+                line = <span key={i}>{line}</span>;
+
+                if (i === arr.length - 1) {
+                  return line;
+                } else {
+                  return [line, <br key={i + 'br'} />];
                 }
-            );
-        }
-    };
-
-    render() {
-        const {
-            children,
-            more,
-            less,
-            lines,
-            anchorClass,
-            width,
-            keepNewLines
-        } = this.props;
-
-        const { expanded, truncated } = this.state;
-
-        return (
-            <div>
-                <Truncate
-                    width={width}
-                    lines={!expanded && lines}
-                    ellipsis={
-                        <span>
-                            ...{' '}
-                            <a
-                                href='#'
-                                className={anchorClass}
-                                onClick={this.toggleLines}
-                            >
-                                {more}
-                            </a>
-                        </span>
-                    }
-                    onTruncate={this.handleTruncate}
-                    ref={ref => (this.truncateRef = ref)}
-                >
-                    {keepNewLines ?
-                        children.split('\n').map((line, i, arr) => {
-                            line = <span key={i}>{line}</span>;
-
-                            if (i === arr.length - 1) {
-                                return line;
-                            } else {
-                                return [line, <br key={i + 'br'} />];
-                            }
-                        }
-                        )
-                        : children
-                    }
-                </Truncate>
-                {!truncated && expanded && (
-                    <span>
-                        {' '}
-                        <a
-                            href='#'
-                            className={anchorClass}
-                            onClick={this.toggleLines}
-                        >
-                            {less}
-                        </a>
-                    </span>
-                )}
-            </div>
-        );
-    }
+              })
+            : children}
+        </Truncate>
+        {!truncated && expanded && (
+          <span>
+            {' '}
+            <a href='#' className={anchorClass} onClick={this.toggleLines}>
+              {less}
+            </a>
+          </span>
+        )}
+      </div>
+    );
+  }
 }
 
 export default ShowMoreText;
